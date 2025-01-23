d53b8e897100202918a4a9543030ec32b19b1e78
mattsimkins <simkinsmatt@hotmail.com>
2025-01-23 02:11:08 -0800

    Added the file_name of the model as an argument for read/write functions. This allows the caller to specify different model names.

--- src/file_handling.cpp
+++ src/file_handling.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
 #include <stdio.h>
 #include <sstream>
 #include "file_handling.h"
@@ -17,12 +14,12 @@ vector<vector<float>> read_traj(string file_name) {
         std::string line;
 
         // Read the file line by line
-        while (std::getline(inputFile, line)) {
-            
+        while (std::getline(inputFile, line)) {            
             stringstream ss(line);
             string word;
             string x_value = "";
             string y_value = "";
+
             while (getline(ss, word, ' ')) { // Use space as delimiter
                 if (x_value != ""){
                     y_value = word;
@@ -34,94 +31,34 @@ vector<vector<float>> read_traj(string file_name) {
             std::vector<float> newRow = {stof(x_value), stof(y_value)};
             traj.push_back(newRow); // Append the new row to the matrix
         }
-        /*
-        // Print the matrix
-        for (const auto &row : traj) {
-            for (int val : row) {
-                std::cout << val << " ";
-            }
-        cout << endl;
-        }
-        */
 
         inputFile.close();
     } else {
-        cerr << "Error: Unable to open file." << std::endl;
+        cerr << "Error: Unable to open file." << endl;
     }
     return traj;
 }
 
-// For reading and writting model to a file
-struct Model_R_W {
-    float d;
-
-    float average_path_length;
-    int grid_update_count;
-    int max_coord_count;
-    float shortest_segment;
-
-    float start_1;
-    float start_2;
-
-    float shift_1;
-    float shift_2;
-
-    // Grid of variable dimensions
-    int size_i;
-    int size_j;
-    int size_k;
-    string grid_string; // 3D vector marshaled as string
-    void loadFromFile(ifstream &infile) {
-        infile >> d;
-
-        infile >> average_path_length;
-        infile >> grid_update_count;
-        infile >> max_coord_count;
-        infile >> shortest_segment;
-
-        // Two elements of last_start_pt vector
-        infile >> start_1;
-        infile >> start_2;
-
-        // Two elements of shift vector
-        infile >> shift_1;
-        infile >> shift_2;
-
-        // Not part of model, but for read/write purposes
-        infile >> size_i;
-        infile >> size_j;
-        infile >> size_k;
-
-        infile.ignore(1, ' ');
-        getline(infile, grid_string);
-    }
-};
-
-
-
-void save_model(BuildGrid::GridInfo* model_ptr){
+void save_model(BuildGrid::GridInfo* model_ptr, string& file_name){
     
-    // Copy to type Model_R_W to martial vectors for read/write 
+    // Copy to type Model_R_W and martial vectors for write 
     Model_R_W model_w;
     model_w.d = model_ptr->d;
     model_w.average_path_length = model_ptr->average_path_length;
     model_w.grid_update_count = model_ptr->grid_update_count;
     model_w.max_coord_count = model_ptr->max_coord_count;
     model_w.shortest_segment = model_ptr->shortest_segment;
-    
     model_w.start_1 = model_ptr->last_start_pt[0];
     model_w.start_2 = model_ptr->last_start_pt[1];
-
     model_w.shift_1 = model_ptr->shift[0];
     model_w.shift_2 = model_ptr->shift[1];
-
     model_w.size_i = model_ptr->grid.size();
     model_w.size_j = model_ptr->grid[0].size();
     model_w.size_k = model_ptr->grid[0][0].size();
     
     // String indexing for grid
     string str_val;
-    string marshaled_vec; // String containing indices and angles
+    string marshaled_vec;
     for (int i = 0; i < model_w.size_i; ++i){
         for (int j = 0; j < model_w.size_j; ++j) {
             for (int k = 0; k < model_w.size_k; ++k){
@@ -134,13 +71,11 @@ void save_model(BuildGrid::GridInfo* model_ptr){
     }
     model_w.grid_string = marshaled_vec;
     
-    ofstream outfile("model.txt");
+    ofstream outfile(file_name);
 
   if (!outfile.is_open()) {
         cerr << "Error opening file!" << endl;
     }
-
-
 
     outfile << model_w.d << " " 
             << model_w.average_path_length << " "
@@ -156,21 +91,18 @@ void save_model(BuildGrid::GridInfo* model_ptr){
             << model_w.size_k << " "
             << model_w.grid_string << endl;
     outfile.close();
-
 }
 
+BuildGrid::GridInfo* read_model(string file_name) {
 
-BuildGrid::GridInfo* read_model() {
+    BuildGrid::GridInfo* model = new BuildGrid::GridInfo;
 
-    BuildGrid::GridInfo* model = new BuildGrid::GridInfo; // !!Assess memory usage with this
+    Model_R_W model_r; // Structure used for reading
 
-    Model_R_W model_r; // Uses read/write structure
-    //BuildGrid::GridInfo model_return; // Uses structure for training and estimation
-
-    std::ifstream infile("model.txt");
+    std::ifstream infile(file_name);
     model_r.loadFromFile(infile);
 
-    // Growing model_return dynamically in nested loops would be more space efficient!!
+    // Vector to populate from string in saved grid
     vector<vector<vector<float>>> marsheled_grid(
                                         model_r.size_i, vector<vector<float>>(
                                         model_r.size_j, vector<float>(
@@ -201,8 +133,6 @@ BuildGrid::GridInfo* read_model() {
         }
     }
 
-     
-
     model->d = model_r.d;
     model->average_path_length = model_r.average_path_length;
     model->grid_update_count = model_r.grid_update_count;
@@ -210,9 +140,7 @@ BuildGrid::GridInfo* read_model() {
     model->shortest_segment = model_r.shortest_segment;
     model->last_start_pt = {model_r.start_1, model_r.start_2};
     model->shift = {model_r.shift_1, model_r.shift_2};
-    model->grid = marsheled_grid; // !!not very memory efficient
-
-
+    model->grid = marsheled_grid;
 
     return model;
 }
