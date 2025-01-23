b51a26a846450de23fbdd9628d00f14e5be9ea51
mattsimkins <simkinsmatt@hotmail.com>
2025-01-23 03:21:32 -0800

    Refactoring

--- src/build_grid.cpp
+++ src/build_grid.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <cmath> 
-#include <Eigen/Dense> // Not sure I'll use this
-#include <vector>
 #include <limits>
 #include <cmath>
 #include "build_grid.h"
@@ -10,39 +8,35 @@
 using namespace std;
 
 
-// Constructor implementation
 BuildGrid::BuildGrid(vector<vector<float>>& trajectory, float& node_spacing)
                 : traj_c(trajectory) {
-    model.d = node_spacing; // Node spacing
-    model.grid_update_count = 0; // Number of times grid has been updated
-    model.average_path_length = 0; //Average length of training paths
-    model.max_coord_count = 0; // Max quantity of cooordinates among trajectories
-    model.shortest_segment = model.d; // Min distance between trajectory coordinates
-    model.last_start_pt = trajectory[0]; // Retains a viable start point for model
-
-    // Calculating extents are done redundantly in trainGrid for only new, empty grids
+        model.d = node_spacing;
+    model.grid_update_count = 0;
+    model.average_path_length = 0;
+    model.max_coord_count = 0;
+    model.shortest_segment = model.d;
+    model.last_start_pt = trajectory[0];
     cSpaceExtents = calculateExtents_c();
-    model.shift = {-cSpaceExtents[0], -cSpaceExtents[2]}; // C-Space to S-Space vector
-    initializeGrid(); // Initialize empty grid with extents and spacing
-    trainGrid(traj_c); // Change to using pointers!!!!
+    model.shift = {-cSpaceExtents[0], -cSpaceExtents[2]};
+    initializeGrid();
+    trainGrid(traj_c);
 }
 
 BuildGrid::~BuildGrid() {}
 
-// Public function
+// Public functions
 BuildGrid::GridInfo* BuildGrid::getModel() {
     return &model;
 }
 
-// Public function
 void BuildGrid::trainOnTrajectory(vector<vector<float>>& traj){
     trainGrid(traj);
 }
 
-// Public function
 vector<float> BuildGrid::getStartPoint() {
     return model.last_start_pt;
 }
+
 
 void BuildGrid::fixDuplicates(){
     // Check for duplicate points in the trajectory
@@ -58,7 +52,9 @@ void BuildGrid::fixDuplicates(){
 // Increment grid size if trajectory exceeds current extents by increments of d
 void BuildGrid::checkExtents_c(){
     vector<float> trajExtents_c = calculateExtents_c();
-    vector<int> expandIncrement;
+    
+    // Check if current training trajectory fits within S-space
+    vector<int> expandIncrement; // Amount S-space is possibly enlarged
     float sign = -1;
     for (int i = 0; i < trajExtents_c.size(); ++i) {
         float diff = trajExtents_c[i] - cSpaceExtents[i];
@@ -72,7 +68,7 @@ void BuildGrid::checkExtents_c(){
         sign = sign * -1;
     }
     if (expandIncrement[0] > 0 || expandIncrement[1] > 0 ||
-                            expandIncrement[2] > 0 || expandIncrement[3] > 0){
+                            expandIncrement[2] > 0 || expandIncrement[3] > 0) {
         cSpaceExtents[0] -= expandIncrement[0]*model.d;
         cSpaceExtents[1] += expandIncrement[1]*model.d;
         cSpaceExtents[2] -= expandIncrement[2]*model.d*Y_TRI;
@@ -80,8 +76,14 @@ void BuildGrid::checkExtents_c(){
         
         // Save old grid and initialize new, resized grid
         vector<vector<vector<float>>> old_grid = model.grid;
-        model.shift = {-cSpaceExtents[0], -cSpaceExtents[2]}; // from C-Space to S-Space
+
+        // Calculate C-Space to S-Space transformation vector
+        model.shift = {-cSpaceExtents[0], -cSpaceExtents[2]};
+
+        // Generate an grid large enough to span S-space
         initializeGrid();
+
+        // Enlarging S-space with larger grid
         for (int i = 0; i < old_grid.size(); i++) {
             for (int j = 0; j < old_grid[0].size(); j++) {
                 for (int k = 0; k < old_grid[0][0].size(); k++) {
@@ -91,36 +93,31 @@ void BuildGrid::checkExtents_c(){
             }
         }
     }
+
+    // Obtain new shift vector for enlarged space
     model.shift = {-cSpaceExtents[0], -cSpaceExtents[2]};
 }
 
 vector<float> BuildGrid::calculateExtents_c(){
-    int columnIndex = 0;
-    
-    // Initialize with extreme positive and negative floats
+ 
+    // Find x extents
     float minX = numeric_limits<float>::max();
     float maxX = numeric_limits<float>::lowest();
-    
     for (const auto& row : traj_c) {
-        if (columnIndex < row.size()) {
-            minX = min(minX, row[columnIndex]);
-            maxX = max(maxX, row[columnIndex]);
-        }
+        minX = min(minX, row[0]);
+        maxX = max(maxX, row[0]);
     }
 
     
-    // Initialize with extreme positive and negative floats
+    // Find y extents
     float minY = numeric_limits<float>::max();
     float maxY = numeric_limits<float>::lowest();
-    columnIndex = 1;    
     for (const auto& row : traj_c) {
-        if (columnIndex < row.size()) {
-            minY = min(minY, row[columnIndex]);
-            maxY = max(maxY, row[columnIndex]);
-        }
+        minY = min(minY, row[1]);
+        maxY = max(maxY, row[1]);
     }
      
-    // Include padding to the extents
+    // Add padding to the extents
     vector<float> trajExtents = {
                                 minX - model.d,
                                 maxX + model.d,
@@ -131,7 +128,10 @@ vector<float> BuildGrid::calculateExtents_c(){
 }
 
 //  Initialize grid sized for trajectory
-vector<vector<vector<float>>> BuildGrid::sizeGrid(const int& rows, const int& cols, const int& depth) {
+vector<vector<vector<float>>> BuildGrid::sizeGrid(
+                                                  const int& rows,
+                                                  const int& cols,
+                                                  const int& depth) {
     vector<vector<vector<float>>> resized_grid(
                                             rows, vector<vector<float>>(
                                             cols, vector<float>(
@@ -139,21 +139,19 @@ vector<vector<vector<float>>> BuildGrid::sizeGrid(const int& rows, const int& co
     return resized_grid;
     }
 
-// Grid must be initialized with a trajectory and node spacing
+// Initialize grid with a trajectory for size, and node spacing
 void BuildGrid::initializeGrid() {
     gridSizeX = cSpaceExtents[1] - cSpaceExtents[0];
     gridSizeY = cSpaceExtents[3] - cSpaceExtents[2];
-    int count_i = ceil(gridSizeX/model.d); //Along x direction in C-Space
-    int count_j = ceil(gridSizeY/(model.d*Y_TRI)); //Along y direction in C-Space
-    const int count_k = 2; //x and y component of vector
-    model.grid = sizeGrid(count_i, count_j, count_k);
+    int count_i = ceil(gridSizeX/model.d); //Along x in C-Space
+    int count_j = ceil(gridSizeY/(model.d*Y_TRI)); //Along y in C-Space
+    model.grid = sizeGrid(count_i, count_j, 2);
 }
 
-// Find the three closest nodes to the given trajectory and update the grid
-void BuildGrid::trainGrid(const vector<vector<float>>& traj) {
-    traj_c = traj; // Initialize C-Space trajectory
+// Find the three closest nodes to the given trajectory coordinate
+void BuildGrid::trainGrid(const vector<vector<float>>& traj_c) {
     fixDuplicates(); // Removes duplicate, consecutive trajectory points
-    checkExtents_c(); // Check if traj_c requires a larger grid and resize if necessary
+    checkExtents_c(); // Check if traj_c requires a larger grid
     
     // Calculate metrics for the trajectory
     metrics traj_metrics = trajectory_metrics(traj_c);
@@ -180,6 +178,7 @@ void BuildGrid::trainGrid(const vector<vector<float>>& traj) {
         row[1] += model.shift[1];
     }
 
+    // Loop through trajectory to update grid
     int next_ind = 0;
     for (const auto& loc : traj_s) {
         
@@ -215,38 +214,46 @@ void BuildGrid::trainGrid(const vector<vector<float>>& traj) {
             vector<float> loc_center = coord_from_ind(trident[2], model.d);
             
             // Calculate vector from nodes to trajectory
-            vector<float> vec_left2traj_next = subtract_vectors(loc_next, loc_left);
-            vector<float> vec_right2traj_next = subtract_vectors(loc_next, loc_right);
-            vector<float> vec_center2traj_next = subtract_vectors(loc_next, loc_center);
+            vector<float> vec_left2traj_next = subtract_vectors(loc_next,
+                                                                loc_left);
+            vector<float> vec_right2traj_next = subtract_vectors(loc_next,
+                                                                 loc_right);
+            vector<float> vec_center2traj_next = subtract_vectors(loc_next,
+                                                                  loc_center);
             
             // Upate nodes with trajectory
             updateNode(vec_left2traj_next, trident[0]);
             updateNode(vec_right2traj_next, trident[1]);
             updateNode(vec_center2traj_next, trident[2]);
         
-        // Next location is far, must update nodes in between
+        // If next trajectory point is distant update nodes in between
         } else {
 
                 // Find direction of increment
-                vector<float> vec_hat_vec2next_traj_pt = 
-                        scalar_multiply(vec2next_traj_pt, 1/vec_len_vec2next_traj_pt);
+                vector<float> vec_direction = 
+                        scalar_multiply(vec2next_traj_pt,
+                        1/vec_len_vec2next_traj_pt);
                     
                 // Divide current vector into peices to increment
                 int n_inc = floor(vec_len_vec2next_traj_pt/model.d);
 
-                // Creep along long distance, populate nodes along the way   
+                // Step along long distance, populate nodes along the way
                 for (int j = 0; j < n_inc; j++) {
-                    float len_of_vec_increment = (float)j*model.d;
-                    vector<float> loc_of_increment = add_vectors(loc,
-                            scalar_multiply(vec_hat_vec2next_traj_pt,
+                    float len_of_vec_increment = (float)j * model.d;
+                    vector<float> inc_loc = add_vectors(loc,
+                            scalar_multiply(vec_direction,
                             len_of_vec_increment));
                     
-                    vector<vector<int>> trident = find_trident(loc_of_increment, model.d);
+                    vector<vector<int>> trident = find_trident(inc_loc,
+                                                               model.d);
                     
                     // Obtain coordinates of nodes
-                    vector<float> loc_left = coord_from_ind(trident[0], model.d);
-                    vector<float> loc_right = coord_from_ind(trident[1], model.d);
-                    vector<float> loc_center = coord_from_ind(trident[2], model.d);
+                    vector<float> loc_left = coord_from_ind(trident[0],
+                                                            model.d);
+                    vector<float> loc_right = coord_from_ind(trident[1],
+                                                             model.d);
+                    vector<float> loc_center = coord_from_ind(trident[2],
+                                                              model.d);
                     
                     // Calculate vectors from nodes to next point on trajectory
                     vector<float> vec_left2traj_next = 
@@ -266,7 +273,8 @@ void BuildGrid::trainGrid(const vector<vector<float>>& traj) {
 }
 
 
-void BuildGrid::updateNode(const vector<float>& vec, const vector<int>& indices) {
+void BuildGrid::updateNode(const vector<float>& vec,
+                           const vector<int>& indices) {
     
     int i = indices[0];
     int j = indices[1];
@@ -278,11 +286,12 @@ void BuildGrid::updateNode(const vector<float>& vec, const vector<int>& indices)
     
     // For nodes not empty, update must consider previous value
     } else {
+        
         // Convert vector in grid to np vector
         vector<float> hist_vec = {model.grid[i][j][0], model.grid[i][j][1]};
         
         // Dot current vector into vector saved in node
-        float dot_prod = vec[0]*hist_vec[0] + vec[1]*hist_vec[1];
+        float dot_prod = vec[0] * hist_vec[0] + vec[1] * hist_vec[1];
         
         // Length update vector
         float len_vec = norm(vec, {0, 0});
@@ -291,7 +300,7 @@ void BuildGrid::updateNode(const vector<float>& vec, const vector<int>& indices)
         float len_hist_vec = norm(hist_vec, {0, 0});
         
         // Find the angle between both vectors
-        float cos_theta = dot_prod / (len_vec*len_hist_vec);
+        float cos_theta = dot_prod / (len_vec * len_hist_vec);
         float angle_rad = acos(cos_theta); // Angle in radians
         
         // Direction has changed, overwrite node
