1d97a5cb92622b27a9d88932a1d72b5810cb00f2
mattsimkins <simkinsmatt@hotmail.com>
2025-01-26 14:21:02 -0800

    Moving functions out of path functions. Functions were moved out of module up to the find_triad() function.

--- src/build_grid.cpp
+++ src/build_grid.cpp
@@ -4,13 +4,14 @@
 #include <cmath>
 #include "build_grid.h"
 #include "path_functions.h"
+#include "find_path.h"
 
 using namespace std;
 
 
 BuildGrid::BuildGrid(vector<vector<float>>& trajectory, float& node_spacing)
                 : traj_c(trajectory) {
-        model.d = node_spacing;
+    model.d = node_spacing;
     model.grid_update_count = 0;
     model.average_path_length = 0;
     model.max_coord_count = 0;
@@ -29,14 +30,22 @@ BuildGrid::GridInfo* BuildGrid::getModel() {
     return &model;
 }
 
-void BuildGrid::trainOnTrajectory(vector<vector<float>>& traj){
-    trainGrid(traj);
-}
 
 vector<float> BuildGrid::getStartPoint() {
     return model.last_start_pt;
 }
 
+// Member functions
+void BuildGrid::trainOnTrajectory(vector<vector<float>>& traj){
+    trainGrid(traj);
+
+    //Estimate trajectory to refine model
+    FindPath estimate(getModel(), model.last_start_pt);
+    //trainGrid(traj);
+    estimate.find_path(model.last_start_pt);
+    trainGrid(traj);
+    
+}
 
 void BuildGrid::fixDuplicates(){
     // Check for duplicate points in the trajectory
@@ -320,4 +329,179 @@ void BuildGrid::updateNode(const vector<float>& vec,
             model.grid[i][j][1] = updated_vec[1];
         }
     }
+}
+
+vector<int> find_closest_index(const vector<float>& coord, const float& d){
+    int j_closest = round(coord[1] / (d * Y_TRI));
+    int i_closest = round(coord[0] / d - 0.5);
+    if (j_closest % 2 == 0) {
+        int i_closest = round(coord[0] / d);
+    }
+    return {i_closest, j_closest};
+}
+
+vector<float> index2coord(const vector<int>& index, const float& d){
+    vector<float> coord = {(float)index[0] * d, (float)index[1] * d * Y_TRI};
+    if (index[1] % 2 != 0) {
+        coord[0] += d / 2;
+    }
+    return coord;
+}
+
+vector<float> coord_from_ind(const vector<int>& ind, const float& d){
+    float x;
+    if (ind[1] % 2 == 0) {
+            x = ind[0]*d;
+        } else { 
+            x = ind[0]*d + d/2;
+        }
+    
+    float y = ind[1]*d*Y_TRI;
+    return {x, y};
+}
+
+
+// Returns 3 rows of indices, to the left, right, and center of coordinate
+vector<vector<int>> find_trident(const vector<float>& coord, const float& d) {
+
+    vector<int> ci = find_closest_index(coord, d); // Closest index (ci)
+
+    // 6 directions for hexagonal grid of equatorial triangles
+    const vector<vector<float>> unit_vecs = {{-1, 0}, 
+                                             {-0.5, Y_TRI},
+                                             {0.5, Y_TRI},
+                                             {1, 0},
+                                             {0.5, -Y_TRI},
+                                             {-0.5, -Y_TRI}
+                                             };
+
+    // Determine if closest node is on a shifted row in x direction
+    bool closest_shifted = true;
+    if (ci[1] % 2 == 0){
+        closest_shifted = false;
+    }
+
+    // Find the coordinate of closest node (ccn) given coordinate on trajectory
+    vector<float> ccn = index2coord(ci, d);
+
+    // Evaluate distances by dotting coorinate into unit vectors
+    float dot1 = 0, dot2 = 0; // Largest, and second largest dot product
+    int ind1 = 0, ind2 = 0; // Index of largest, and second largest dot product
+    
+    // Find the two largest dot products
+    for (int i = 0; i < unit_vecs.size(); i++) {
+        float dot_check = (coord[0] - ccn[0])*unit_vecs[i][0] +
+                    (coord[1] - ccn[1])*unit_vecs[i][1];
+        if (dot_check > dot2){
+            dot2 = dot_check;
+            ind2 = i;
+        }
+        if (dot2 > dot1){
+            float dot_largest = dot2;
+            int unit_vec_largest = ind2;
+            dot2 = dot1;
+            ind2 = ind1;
+            dot1 = dot_largest;
+            ind1 = unit_vec_largest;
+        }
+    }
+    /*
+    The rest of the funtion is based on spacial reasoning. In words, the
+    closest node is encircled by 6 nodes that collectively form a hexagon.
+    The trajectory point falls within one of the 6 sectors defined by the
+    hexagon. The two largest dot products are used to determine the sector
+    in which the trajectory point falls. The sector is defined by the two
+    unit vectors that form the sector's boundaries. The closest node is
+    the center of the hexagon, and the two largest dot products are used
+    to determine the sector in which the trajectory point falls.
+    */
+    
+    // Arbitrarily picking sextant I, all are equally valid
+    if (ind1 == 0 && ind2 == 0){
+        ind2 = 1;
+    }
+
+    int i_left = 0, j_left = 0, i_right = 0, j_right = 0, i_center = 0, j_center = 0;
+
+    // Define sextant I based on dot product
+    if ((ind1 == 0 && ind2 == 1) || (ind1 == 1 && ind2 == 0)){
+        i_left = ci[0] - 1;
+        j_left = ci[1];
+        i_right = ci[0];
+        if (closest_shifted) {
+            i_center = ci[0];
+        } else { 
+            i_center = ci[0] - 1;
+        }
+        j_center = ci[1] + 1;
+
+    // Define sextant II based on dot product
+    } else if ((ind1 == 1 && ind2 == 2) || (ind1 == 2 && ind2 == 1)){
+        if (closest_shifted) {
+            i_left = ci[0];
+        } else {
+            i_left = ci[0] - 1;
+        }
+        j_left = ci[1] + 1;
+        i_right = i_left + 1;
+        i_center = ci[0];
+        j_center = ci[1];
+    
+
+    // Define sextant III based on dot product
+    } else if ((ind1 == 2 && ind2 == 3) || (ind1 == 3 && ind2 == 2)){
+        i_left = ci[0];
+        j_left = ci[1];
+        i_right = ci[0] + 1;
+        if (closest_shifted){
+            i_center = ci[0] + 1;
+        } else {
+            i_center = ci[0];
+        }
+        j_center = ci[1] + 1;
+
+        
+    // Define sextant IV based on dot product
+    } else if ((ind1 == 3 && ind2 == 4) || (ind1 == 4 && ind2 == 3)){
+        i_left = ci[0];
+        j_left = ci[1];
+        i_right = ci[0] + 1;
+        if (closest_shifted){
+            i_center = ci[0] + 1;
+        } else{
+            i_center = ci[0];
+        }
+        j_center = ci[1] - 1;
+
+    // Define sextant V based on dot product    
+    } else if ((ind1 == 4 && ind2 == 5) || (ind1 == 5 && ind2 == 4)){
+        if (closest_shifted){
+            i_left = ci[0];
+        } else {
+            i_left = ci[0] - 1;
+        }
+        j_left = ci[1] - 1;
+        i_right = i_left + 1;
+        i_center = ci[0];
+        j_center = ci[1];
+        
+    // Define sextant VI based on dot product    
+    } else if ((ind1 == 5 && ind2 == 0) || (ind1 == 0 && ind2 == 5)){
+        i_left = ci[0] - 1;
+        j_left = ci[1];
+        i_right = ci[0];
+        if (closest_shifted){
+            i_center = ci[0];
+        } else {
+            i_center = ci[0] - 1;
+        }
+        j_center = ci[1] - 1;
+    }
+
+    vector<vector<int>> triad = {
+                                {i_left, j_left},
+                                {i_right, j_left},
+                                {i_center, j_center}
+                                };
+    return triad;
 }
