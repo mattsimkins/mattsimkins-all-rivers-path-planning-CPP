db27e2810e5b5f1a1945e6f8702e8a27f010581d
mattsimkins <simkinsmatt@hotmail.com>
2025-01-27 10:27:09 -0800

    Moved header files into an include directory.

---
+++ include/build_grid.h
@@ -0,0 +1,117 @@
+#ifndef BUILD_GRID
+#define BUILD_GRID
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+const float Y_TRI = sqrt(3)/2; // Height of an equilateral triangle
+const float ANGLE_THRESHOLD = 40*M_PI/180; // Maximum angle difference between new and old vector
+
+/*
+This class estimates trajectories from a grid. Importantly, it does not just
+use the grid to estimate a path, it also updates the grid with new, calculated
+trajectories. Therefore, there's arguably a weak dependency between the 
+BuildGrid and FindPath classes because FindPath has some elements of training.
+*/
+
+class BuildGrid {
+public:
+
+    // Zero argument constructor for accessing public functions
+    BuildGrid();
+    
+    // Constructor for new grid
+    BuildGrid(vector<vector<float>>& traj_c, float& d);
+    
+    // Training on existing grid
+    void trainOnTrajectory(vector<vector<float>>& traj);
+    
+    // Captures essential training model information
+    struct GridInfo {
+        float d; // Node spacing
+        float average_path_length; // Average length of training paths
+        int grid_update_count; // Number of times grid has been updated
+        int max_coord_count; // Max coordinate count in training trajectories
+        float shortest_segment; // Min distance between trajectory coordinates
+        vector<float> last_start_pt; // Retains a viable start point for model
+        vector<float> shift; // Shifts C-Space to S-Space
+        vector<vector<vector<float>>> grid; // 3D matrix to store grid values
+    };
+
+
+    GridInfo* getModel(); // Member function to return model pointer
+    vector<float> getStartPoint(); // Retains a valid start point
+
+    //Adds two vectors element-wise
+    vector<float> add_vectors(vector<float> v1, vector<float> v2);
+
+    // Subtracts two vectors element-wise
+    vector<float> subtract_vectors(vector<float> v1, vector<float> v2);
+
+    // Calculates the Euclidean distance between two vectors
+    float norm(vector<float> v1, vector<float> v2);
+
+    // Calculate scalar multiply of vector
+    vector<float> scalar_multiply(vector<float> v, float scalar);
+
+    // Calculate distance between a location, and the coordinate of a node given by its indices
+    float dist2node(vector<float> loc, vector<int> indices, float d);
+
+    // Calculates the coordinates of a node given its index
+    vector<float> index2coord(vector<int> index, float d);
+
+    // Finds the three closest nodes to the given coordinate
+    vector<vector<int>> find_trident(vector<float> coord, float d);
+
+    // Determines if a coordinate is outside the grid
+    bool outsideExtents(vector<float> coord, int x_extent,
+                        int y_extent, float d);
+
+    // Throws error if a coordinate is outside the grid
+    bool checkExtents(vector<float> coord, int x_extent,
+                        int y_extent, float d);
+
+    // Finds the index of the closest node to the given coordinate
+    vector<int> find_closest_index(vector<float> coord, float d);
+
+    // Find S-Space coordinates given index
+    vector<float> coord_from_ind(vector<int> ind, float d);
+
+    // Trajectory metrics are for later use when estimating a path from grid
+    struct metrics {
+        float path_length;
+        float shortest_segment;
+        float coord_count;
+    };
+    metrics trajectory_metrics(vector<vector<float>> traj);
+
+
+    ~BuildGrid();
+
+
+private:
+    void fixDuplicates(); // Removes consecutive duplicate points from trajectory
+    void initializeGrid(); // Initializes the grid for first training trajectory
+    void trainGrid(const vector<vector<float>>& traj); // Trains the grid with the trajectory in c-space
+    void checkExtents_c(); // Resizes grid if a training trajectory exceeds old extents
+    void updateNode(const vector<float>& vec, const vector<int>& indices);
+    GridInfo model;
+    //GridInfo* model_ptr = &model;
+    vector<float> calculateExtents_c(); // Calculates C-Space extents of trajectory
+    vector<vector<vector<float>>> sizeGrid(const int& rows, const int& cols, const int& depth); // Resizes the grid
+    vector<float> cSpaceExtents; // Coordinate frame extents in C-Space
+    int grid_update_count; // Number of times grid has been updated
+    float average_path_length;
+    int max_coord_count;
+    float shortest_segment;
+    vector<float> sSpaceExtents;
+    float d;
+    float gridSizeX;
+    float gridSizeY;
+    float variable;
+    vector<vector<float>> traj_c;
+    vector<vector<float>> traj_s;
+};
+
+#endif
---
+++ include/file_handling.h
@@ -0,0 +1,68 @@
+#ifndef FILE_HANDLING 
+#define FILE_HANDLING
+#include <string>
+#include <vector>
+#include <fstream>
+#include "build_grid.h"
+
+using namespace std;
+
+// Read in a trajectory from .txt file
+vector<vector<float>> read_traj(string file_name);
+
+// Write a trajectory to a .txt file
+void write_traj(vector<vector<float>>& traj, string file_name); 
+
+// Save model, vectors must be marshaled to strings
+void save_model(BuildGrid::GridInfo* trained_model, string file_name);
+
+// Read model, strings must be marshaled to vectors
+BuildGrid::GridInfo* read_model(string file_name);
+
+// For reading and writting model to a file
+struct Model_R_W {
+    float d;
+    float average_path_length;
+    int grid_update_count;
+    int max_coord_count;
+    float shortest_segment;
+
+    // Start and shift vectors are stored as floats
+    float start_1;
+    float start_2;
+    float shift_1;
+    float shift_2;
+
+    // Vector grid size varies, save dimensions for reading
+    int size_i;
+    int size_j;
+    int size_k;
+    string grid_string; // 3D vector marshaled as string
+    
+    // Retain locations in struct
+    void loadFromFile(ifstream &infile) {
+        infile >> d;
+        infile >> average_path_length;
+        infile >> grid_update_count;
+        infile >> max_coord_count;
+        infile >> shortest_segment;
+
+        // Two elements of last_start_pt vector
+        infile >> start_1;
+        infile >> start_2;
+
+        // Two elements of shift vector
+        infile >> shift_1;
+        infile >> shift_2;
+
+        // Not part of model, but for read/write purposes
+        infile >> size_i;
+        infile >> size_j;
+        infile >> size_k;
+
+        infile.ignore(1, ' ');
+        getline(infile, grid_string);
+    }
+};
+
+#endif
